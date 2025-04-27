c2532bedb415fb6307af33dc83e7214f99108621
ahmed kasban <ahmedkasban520@gmail.com>
2025-04-27 16:14:49 +0300

    Some edit

--- Gym_Management_System/Model/Stuff/Member/member.cpp
+++ Gym_Management_System/Model/Stuff/Member/member.cpp
@@ -4,6 +4,16 @@ Member::Member(int Id, std::string Name, std::string Phone, stDate BirthDate, st
                std::string Password, Subscription subscription, std::vector<Workout> workouts)
     : Person(Name, Phone, BirthDate, UserName, Password)
 {
+    this->Id=Id;
     this->subscription = subscription;
     this->Workouts = workouts;
 }
+
+Member::Member(std::string Name, std::string Phone, stDate BirthDate, std::string UserName,
+       std::string Password, Subscription subscription)
+    : Person(Name, Phone, BirthDate, UserName, Password)
+{
+    this->subscription = subscription;
+
+}
+
