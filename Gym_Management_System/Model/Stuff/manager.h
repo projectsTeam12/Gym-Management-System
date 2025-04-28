db20433977cca1505dcef4ec6d9a34ae68258ef8
Soliman490 <142852987+Soliman490@users.noreply.github.com>
2025-04-28 16:23:44 +0300

    Update manager.h

--- Gym_Management_System/Model/Stuff/manager.h
+++ Gym_Management_System/Model/Stuff/manager.h
@@ -1,21 +1,14 @@
 #pragma once
-#include<unordered_map>
 #include"person.h"
 #include<vector>
 class Manager : public Person
 {private:
 	double Revenue;
-	int id_emp=0;
-	double salary;
 	string Role;
-	unordered_map<int, string>employrs;
 	vector<string> offers;
 
 public:
-	//Manager(string name, string date, string phone, string username, string password);
-	void Add_emp(string name, string date, string phone, string username, string password,double salary,string role);
-	void find_emp(int id);
-	void Delete_emp(int id);
+	Manager(string name, string date, string phone, string username, string password);
 	void View_Courts();
 	void Viwe_classes();   
 	void add_offers(string offer);
