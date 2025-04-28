ee4873b61413fc71fae0dfc8954e8a196ceed424
Soliman490 <142852987+Soliman490@users.noreply.github.com>
2025-04-28 15:50:10 +0300

    Update manager.h

--- Gym_Management_System/Model/Stuff/manager.h
+++ Gym_Management_System/Model/Stuff/manager.h
@@ -1,14 +1,28 @@
-#ifndef MANAGER_H
-#define MANAGER_H
-
 #pragma once
-#include <iostream>
-#include "Model/Stuff/Person.h"
+#include<unordered_map>
+#include"Person.h"
+#include<vector>
 class Manager : public Person
-{
+{private:
+	double Revenue;
+	int id_emp=0;
+	double salary;
+	string Role;
+	unordered_map<int, string>employrs;
+	vector<string> offers;
+
 public:
-    Manager(string Name, string Phone, stDate BirthDate, string UserName, string Password);
+	//Manager(string name, string date, string phone, string username, string password);
+	void Add_emp(string name, string date, string phone, string username, string password,double salary,string role);
+	void find_emp(int id);
+	void Delete_emp(int id);
+	void View_Courts();
+	void Viwe_classes();   
+	void add_offers(string offer);
+	double calc_rev();
+
+
+
 };
 
 
-#endif // MANAGER_H
