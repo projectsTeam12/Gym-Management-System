972c78ec51ce939978b8e07a2f9d48aed2db6eb1
Soliman490 <142852987+Soliman490@users.noreply.github.com>
2025-04-28 15:48:11 +0300

    Update person.cpp

--- Gym_Management_System/Model/Stuff/person.cpp
+++ Gym_Management_System/Model/Stuff/person.cpp
@@ -1,6 +1,39 @@
-#include "person.h"
-
-Person::Person(string Name, string Phone, stDate BirthDate, string UserName, string Password)
-{
-
+#include "Person.h"
+#include<iostream>
+Person::Person(string name, string date, string phone, string username, string password) {
+	this->Name = name;
+	this->Date = date;
+	this->Phone = phone;
+	this->UserName = username;
+	this->PassWord = password;
+}
+void Person::setName(string name) {
+	Name = name;
+}
+void Person::setDate(string date) {
+	Date = date;
+}
+void Person::setPhone(string phone) {
+	Phone = phone;
+}
+void Person::setUserName(string username) {
+	UserName = username;
+}
+void Person::setPassWord(string password) {
+	PassWord = password;
+}
+string Person::getName() {
+	cout << Name;
+}
+string Person::getDate() {
+	cout << Date;
+}
+string Person::getPhone() {
+	cout << Phone;
+}
+string Person::getUserName() {
+	cout << UserName;
+}
+string Person::getPassWord() {
+	cout<<PassWord;
 }
